d00e866171748283f2bb06a8b21e54aebd48e10d
Matt Deuschle <mdeuschle@gmail.com>
2024-04-28 20:48:05 -0700

    first commit

---
+++ Stash2/Stash2/Stash2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stash2App.swift
+//  Stash2
+//
+//  Created by Matt Deuschle on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Stash2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
